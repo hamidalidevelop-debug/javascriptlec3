51778999ceb62572c012fa99f44245d2cbcdc396
hamidalidevelop-debug <hamidalidevelop@gmail.com>
2026-02-07 03:57:44 +0000

    const var let

--- 01-basic/test.js
+++ 01-basic/test.js
@@ -1,1 +1,18 @@
-console.log("hy")
+const accountId=445465465
+var accoutnEmail="ali@gmail.com"
+let accountPassword="12132132"
+accountCity="faisalabad"
+//  agar koe or field add krni to kr do baad me agar krni ho to 
+let accountState='america'
+
+
+// console.log(accountId);  agar ye ni use krna 
+
+
+// agar ap change krna chate sb to 
+accoutnEmail="ali123@gmail.com"
+accountPassword="121"
+accountCity="Lahore"
+
+
+console.table([accountId,accoutnEmail,accountPassword,accountCity]);
